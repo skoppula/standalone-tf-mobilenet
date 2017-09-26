c6bdf7aba1d4e58b43cfdbb084e59179b6be434f
Skanda Koppula <skoppula@mit.edu>
2017-09-26 10:53:51 -0400

    add imagenet

--- imagenet.py
+++ imagenet.py
@@ -111,8 +111,8 @@ def create_readable_names_for_imagenet_labels():
 
   # pylint: disable=g-line-too-long
   base_url = 'https://raw.githubusercontent.com/tensorflow/models/master/research/inception/inception/data/'
-  synset_url = '{}/imagenet_lsvrc_2015_synsets.txt'.format(base_url)
-  synset_to_human_url = '{}/imagenet_metadata.txt'.format(base_url)
+  synset_url = '{}/descriptors/imagenet_lsvrc_2015_synsets.txt'.format(base_url)
+  synset_to_human_url = '{}/descriptors/imagenet_metadata.txt'.format(base_url)
 
   filename, _ = urllib.request.urlretrieve(synset_url)
   synset_list = [s.strip() for s in open(filename).readlines()]
